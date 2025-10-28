d7e68ae64144ef2caa76eaf123e49fbd16b1a823
jai099 <131276939+jai099@users.noreply.github.com>
2025-10-29 03:33:49 +0530

    initial commit with tailwind configured

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-red-500 to-purple-600 text-white">
+      <h1 className="text-5xl font-bold mb-4">Tailwind CSS is Working 🎉</h1>
+      <p className="text-lg bg-white text-indigo-600 px-4 py-2 rounded-xl shadow-md">
+        If you can see this styled text, Tailwind is successfully configured!
+      </p>
+    </div>
+  );
+}
+
+export default App;
